714ee3f731cc440b07218058f15b66cfebfedefe
Leonardo Moraes <leonardo.moraes@effecti.com.br>
2023-01-14 13:01:19 -0300

    Projeto inicial utilizando gandle gerado. Java 17

---
+++ src/test/java/com/GGoat/agendarservice/AgendarServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GGoat.agendarservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendarServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
